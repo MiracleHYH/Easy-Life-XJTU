469ba4700cbab6ba728dab5fbb738acffbc53aea
Miracle <miracle20001110@gmail.com>
2022-08-18 15:26:50 +0800

    renamed:    conf.yaml -> conf/user.yaml 	modified:   utils/base.py

--- utils/base.py
+++ utils/base.py
@@ -3,5 +3,5 @@ import yaml
 
 class Base:
     def __init__(self) -> None:
-        with open('./conf.yaml', 'r', encoding='utf-8') as f:
-            self.data = yaml.load(f, Loader=yaml.FullLoader)
+        with open('../conf/user.yaml', 'r', encoding='utf-8') as f:
+            self.user = yaml.load(f, Loader=yaml.FullLoader)
